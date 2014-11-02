57eeff99fa573847b52cb4c10c57b9f9ab344896
Kamran Ahmed <kamranahmed.se@gmail.com>
2014-11-02 14:46:07 +0500

    Refactor the populateAddressVars function

--- src/Geocode.php
+++ src/Geocode.php
@@ -103,19 +103,19 @@ class Geocode
         foreach ($this->service_results->results[0]->address_components as $component) {
             if (in_array('street_number', $component->types)) {
                 $this->streetNumber = $component->long_name;
-            } else if (in_array('locality', $component->types)) {
+            } elseif (in_array('locality', $component->types)) {
                 $this->locality = $component->long_name;
-            } else if (in_array('postal_town', $component->types)) {
+            } elseif (in_array('postal_town', $component->types)) {
                 $this->town = $component->long_name;
-            } else if (in_array('administrative_area_level_2', $component->types)) {
+            } elseif (in_array('administrative_area_level_2', $component->types)) {
                 $this->country = $component->long_name;
-            } else if (in_array('country', $component->types)) {
+            } elseif (in_array('country', $component->types)) {
                 $this->country = $component->long_name;
-            } else if (in_array('administrative_area_level_1', $component->types)) {
+            } elseif (in_array('administrative_area_level_1', $component->types)) {
                 $this->district = $component->long_name;
-            } else if (in_array('postal_code', $component->types)) {
+            } elseif (in_array('postal_code', $component->types)) {
                 $this->postcode = $component->long_name;
-            } else if (in_array('route', $component->types)) {
+            } elseif (in_array('route', $component->types)) {
                 $this->streetAddress = $component->long_name;
             }
         }
