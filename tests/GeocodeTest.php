5238fb62e0b1b3c765d1f24dab534926d0fc8f30
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-09-21 12:10:51 +0500

    Confirmation to PSR-2

--- tests/GeocodeTest.php
+++ tests/GeocodeTest.php
@@ -10,28 +10,27 @@ class GeocodeTest extends \PHPUnit_Framework_TestCase
      */
     public function testGeocode($address, $expected)
     {
-        $actual = new Geocode();
+        $actual   = new Geocode();
         $location = $actual->get($address);
-        $methods = array_keys($expected);
+        $methods  = array_keys($expected);
         $this->assertTrue($location->isValid());
         foreach ($methods as $method) {
             $this->assertEquals($expected[$method], $location->$method());
         }
     }
-
-
+    
     public function testInvalidLocation()
     {
-        $geo= new Geocode();
+        $geo      = new Geocode();
         $location = $geo->get("House of the rights for the poor people");
         $this->assertFalse($location->isValid());
- 
     }
+
     /**
-    *@test
-    *@expectedException \Exception
-    *@expectedExceptionMessage Address is required in order to process
-    */
+     * @test
+     * @expectedException \Exception
+     * @expectedExceptionMessage Address is required in order to process
+     */
     public function testEmptyOrNullAdress()
     {
         $actual = new Geocode();
@@ -68,36 +67,36 @@ class GeocodeTest extends \PHPUnit_Framework_TestCase
         );
     }
 
- 
+
     public function providerTestGeocodeProvider()
     {
-        $providers = array();
+        $providers = [];
 
-        $providers[] = array(
-                "1600 Amphitheatre Parkway, Mountain View, CA",
-                array(
-                    'getAddress' => '1600 Amphitheatre Parkway, Mountain View, CA',
-                    'getCountry' => 'United States',
-                    'getLocality' => 'Mountain View',
-                    'getDistrict' => 'California',
-                    'getPostcode' => '94043',
-                    'getStreetAddress' => 'Amphitheatre Parkway',
-                    'getStreetNumber' => '1600'
-                )
-            );
+        $providers[] = [
+            "1600 Amphitheatre Parkway, Mountain View, CA",
+            [
+                'getAddress'       => '1600 Amphitheatre Parkway, Mountain View, CA',
+                'getCountry'       => 'United States',
+                'getLocality'      => 'Mountain View',
+                'getDistrict'      => 'California',
+                'getPostcode'      => '94043',
+                'getStreetAddress' => 'Amphitheatre Parkway',
+                'getStreetNumber'  => '1600',
+            ],
+        ];
 
-        $providers[] = array(
-                "9 Little St, Beachburg, Ontario, Canada",
-                array(
-                    'getAddress' => '9 Little St, Beachburg, Ontario, Canada',
-                    'getCountry' => 'Canada',
-                    'getLocality' => 'Beachburg',
-                    'getDistrict' => 'Ontario',
-                    'getPostcode' => 'K0J 1C0',
-                    'getTown' => '',
-                    'getStreetNumber' => '9'
-                )
-            );
+        $providers[] = [
+            "9 Little St, Beachburg, Ontario, Canada",
+            [
+                'getAddress'      => '9 Little St, Beachburg, Ontario, Canada',
+                'getCountry'      => 'Canada',
+                'getLocality'     => 'Beachburg',
+                'getDistrict'     => 'Ontario',
+                'getPostcode'     => 'K0J 1C0',
+                'getTown'         => '',
+                'getStreetNumber' => '9',
+            ],
+        ];
 
         return $providers;
     }
